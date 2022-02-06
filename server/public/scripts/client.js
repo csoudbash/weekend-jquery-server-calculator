397bad9688c1198923a5e30df9f7e2be9fa22992
Cameron Soudbash <csoudbash@gmail.com>
2022-02-06 16:46:53 -0600

    finished

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,7 +12,7 @@ function onReady() {
     getValues();
 }
 
-// functions assigned 
+// functions that assign an operator based on which operator button was pressed.
 function additionAssignment() {
      operator= '+';
     console.log($('#addition-button').text());
@@ -30,6 +30,8 @@ function divisionAssignment() {
     console.log($('#division-button').text());
 }
 
+// when the equals button is clicked, it runs this function and sends the values to the server side as an object
+// look to the server side and look for an app.post statement with a '/inputs' in it.
 function sendValues() {
     let selectedOperator = operator;
     let numbersToMath = {
@@ -37,7 +39,7 @@ function sendValues() {
         firstNumber: Number($('#first-number').val()),
         secondNumber: Number($('#second-number').val()),
     }
-    console.log(numbersToMath);
+    // console.log(numbersToMath);
     $.ajax({
         // how to send them
          // sending the object to the server
@@ -48,7 +50,7 @@ function sendValues() {
         }
     }).then(function(){
         //what happens when the request had been made
-    //console.log('inputs added correctly!');
+        //console.log('inputs added correctly!');
        getValues();
     }).catch(function(response){
         console.log('rut ro scoob',response);
@@ -59,22 +61,21 @@ function sendValues() {
     $('#first-number').val('');
 
 }    
+
 // getting the finished math object from the server.
 function getValues() {
     $.ajax({
         method: 'GET',
         url: '/mathResults'
     }).then(function(response){
-        // console.log('does this work??');
-        // console.log(response);
-        appendMathToDom(response);// running appendMathToDom with the object 'response' from the server side
+        appendMathToDom(response);// running appendMathToDom with the array of objects 'response' from the server side
         
     })
 }
 
-function appendMathToDom(listOfProblems) {
+function appendMathToDom(listOfProblems) {//function to append the 'response' array of objects to the DOM 
     let lastAnswer = listOfProblems[listOfProblems.length-1];
-
+    console.log(listOfProblems);
     $('#unordered-list').empty();// clears the history of previous math problems each time the function is ran
     $('#last-answer').empty();// clears the history of the previous last answer each time the function is ran
 
@@ -93,16 +94,14 @@ function appendMathToDom(listOfProblems) {
 }
 // function to clear the dom when the C or 'clear' button is pressed
 function clearDom() {
-// console.log('got the clear button working???');
 $.ajax({
         method: 'GET',
         url: '/clearDom'
-    }).then(function(response){
+    }).then(function(response){// response from the server with the updated empty array
         console.log('get has been sent!');
-        // appendMathToDom(response);
         $('#second-number').val('');
         $('#first-number').val('');
-        appendMathToDom(response);
+        appendMathToDom(response);// rerun the function with an empty array 
     }).catch(function(response){
         console.log('rut ro scoob', response);
     })
--- server/server.js
+++ server/server.js
@@ -3,42 +3,51 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
-let previousMathArray = require('./modules/previousMathArray.js');
-let resultsArray = require('./modules/resultsArray');
-// This must be added before GET & POST routes.
+let previousMathArray = require('./modules/previousMathArray.js'); // exporting the module for use on the server side
+// let resultsArray = require('./modules/resultsArray');
 app.use(bodyParser.urlencoded({extended:true}));
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
-// checking if the server is working!
-// console.log('hello!');
+//-----------------------------^^^^^SETTING UP SERVER STUFF^^^^^------------------------------------
 
-app.post('/inputs',function(req,res){
-    console.log('/inputs to get!', req.body.numbersToMath);
-    // previousMathArray.push(req.body.numbersToMath);
-    doMath(req.body.numbersToMath)
-    // let MostRecentInput = req.body.numbersToMath;
-    res.sendStatus(201);
-    // console.log(previousMathArray);
-})
-
-
-app.listen(PORT, () => {
-    console.log ('Server is running on port', PORT)
-  })
-
-
+// NOTES -------------------------------------------------------------------------------------------
   //post response is used as a 'thumbs up' basically saying that the information sent was recieved while the request is the information that waas sent
   // generally exclusivley used as a transfer of data from the client to the server !!! double check this
   //get is used any time data is grabbed from the server or from the client
+// END NOTES ---------------------------------------------------------------------------------------
 
-  function doMath(input) {
-    //   console.log(input);
+// POSTS AND GETS ----------------------------------------------------------------------------------
+
+// grabs the values sent from the client side and sends the object to the 'doMath' function
+app.post('/inputs',function(req,res){
+    console.log('/inputs to get!', req.body.numbersToMath);
+    doMath(req.body.numbersToMath)
+    res.sendStatus(201);
+    // console.log(previousMathArray);
+})
+// sending the entire 'previousMathArray' to the client send when requested to do so.
+app.get('/mathResults',function(req, res){
+    console.log('inside /mathResults', req.body);
+    res.send(previousMathArray);
+})
+// when the server requests a clear of the screen through a button click, we simply set the 'previousMathArray'
+// array to an empty array and then send back the array
+app.get('/clearDom',function(req,res){
+    console.log('resetting dom in progress, beep boop');
+    previousMathArray= [];
+    res.send(previousMathArray); 
+})
+// END POSTS AND GETS ------------------------------------------------------------------------------
+
+// SERVER SIDE FUNCTIONS----------------------------------------------------------------------------
+  function doMath(input) { // function called after the post statement was sent from the client side
       let result;
         // very interesting, so these 2 lines below here are to turn the values from strings into numbers, despite the fact that on the client side
-        // i ensured that they were turned into numbers before being sent over. very interesting....... need to ask liz
+        // i ensured that they were turned into numbers before being sent over. very interesting....... need to ask liz or dane.
       let firstInput = Number(input.firstNumber);
       let secondInput = Number(input.secondNumber);
+
         // math for finding the result of the two numbers
       if (input.operator === '+') {
           result = firstInput + secondInput;
@@ -56,18 +65,12 @@ app.listen(PORT, () => {
           secondNumber: secondInput,
           operator: input.operator,  
       }
-    //   console.log(newObject);
-      previousMathArray.push(newObject);
+      previousMathArray.push(newObject);// updating the 'previousMathArray' module by pushing the new object created to it.
       console.log(result);
-    //   console.log(previousMathArray);
   }
-  app.get('/mathResults',function(req, res){
-      console.log('inside /mathResults', req.body);
-      res.send(previousMathArray);
-  })
-
-  app.get('/clearDom',function(req,res){
-      console.log('resetting dom in progress, beep boop');
-      previousMathArray= [];
-      res.send(previousMathArray); 
+  //  END SERVER SIDE FUNCTIONS---------------------------------------------------------------------
+ 
+  // MORE SERVER SIDE STUFF-----------------------------------------------------------
+  app.listen(PORT, () => {
+    console.log ('Server is running on port', PORT)
   })
